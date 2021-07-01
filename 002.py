6d936dd15f67b6218601a9b20c94ca4a704973a0
Oolonglatte <86589733+Oolonglatte@users.noreply.github.com>
2021-07-01 19:23:58 +0800

    Update 002.py

--- 002.py
+++ 002.py
@@ -1,10 +1,11 @@
 import random,string
-forSelect=string.ascii_letters+'0123456789'
+CharsforSelect=string.ascii_letters + string.digits
+#ascii_letters是生成所有字母，从a-z和A-Z,digits是生成所有数字0-9.
 def generate(count,length):
     for x in range(count):
         re=''
         for y in range(length):
-            re+=random.choice(forSelect)
+            re+=random.choice(CharsforSelect)
         print(re)
 
 if __name__ == '__main__':
